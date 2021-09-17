85923782174747dcf00293f5530e33e6a6a66006
MistyBlunch <gracenikole@gmail.com>
2021-09-16 23:59:19 -0500

    double linked list sorted insert

--- structs/double_linked_list_sorted_insert/main.cpp
+++ structs/double_linked_list_sorted_insert/main.cpp
@@ -4,53 +4,53 @@
 using namespace std;
 
 struct Node {
-    int data;
-    Node* next;
-    Node* prev;
+  int data;
+  Node* next;
+  Node* prev;
 };
 
 struct DoubleList {
-    Node* head;
-    Node* tail;
+  Node* head;
+  Node* tail;
 };
 
 void SortedInsert(DoubleList* list, int data) {
-    Node* nodo = new Node;
-    nodo->data = data;
-    nodo->next = nullptr;
-    nodo->prev = nullptr;
-    if(list->head == nullptr && list->tail == nullptr) {
-        list-> head = nodo;
-        list-> tail = nodo;
-        return;
-    }
+  Node* nodo = new Node;
+  nodo->data = data;
+  nodo->next = nullptr;
+  nodo->prev = nullptr;
+  if(list->head == nullptr && list->tail == nullptr) {
+    list-> head = nodo;
+    list-> tail = nodo;
+    return;
+  }
 
-    Node* i;
-    for(i = list->head; i != nullptr && i->data < data; i = i->next);
+  Node* i;
+  for(i = list->head; i != nullptr && i->data < data; i = i->next);
 
-    if(i != nullptr) {
-        if(i->prev != nullptr) {
-            Node* p = i->prev;
-            p->next = nodo;
-            nodo->prev = p;
-        } else {
-            list->head = nodo;
-        }
-        i->prev = nodo;
-        nodo->next = i;
+  if(i != nullptr) {
+    if(i->prev != nullptr) {
+      Node* p = i->prev;
+      p->next = nodo;
+      nodo->prev = p;
     } else {
-        list->tail->next = nodo;
-        nodo->prev = list->tail;
-        list->tail = nodo;
+      list->head = nodo;
     }
-    
+    i->prev = nodo;
+    nodo->next = i;
+  } else {
+    list->tail->next = nodo;
+    nodo->prev = list->tail;
+    list->tail = nodo;
+  }
+  
 }
 
 void display(DoubleList* head) {
-    cout << " <-> ";
-    for(Node* i = head->head; i != nullptr; i = i->next) 
-        cout << i->data << " <-> ";
-    cout << endl;
+  cout << " <-> ";
+  for(Node* i = head->head; i != nullptr; i = i->next) 
+    cout << i->data << " <-> ";
+  cout << endl;
 }
 
 int main() {
